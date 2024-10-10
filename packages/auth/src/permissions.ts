9f191eb514390d074c784f8aeaef94737cadad0c
Henrique Tavares <ihenrits@gmail.com>
2024-10-10 17:10:55 -0300

    feat(auth): Add zod dependency for input validation

--- packages/auth/src/permissions.ts
+++ packages/auth/src/permissions.ts
@@ -2,20 +2,20 @@ import { AbilityBuilder } from '@casl/ability'
 
 import { AppAbility } from '.'
 import { User } from './models/user'
-
-type Roles = 'ADMIN' | 'MEMBER'
+import { Role } from './roles'
 
 type PermissionByRole = (
   user: User,
   builder: AbilityBuilder<AppAbility>,
 ) => void
 
-export const permissions: Record<Roles, PermissionByRole> = {
+export const permissions: Record<Role, PermissionByRole> = {
   ADMIN(_, { can }) {
     can('manage', 'all')
   },
   MEMBER(_, { can }) {
-    can('invite', 'User')
+    // can('invite', 'User')
     can('manage', 'Project')
   },
+  BILLING() {},
 }
