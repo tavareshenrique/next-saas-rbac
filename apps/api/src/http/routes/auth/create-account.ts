b501af795d0173740b63cbeb9940ed8de1e21f71
Henrique Tavares <ihenrits@gmail.com>
2025-05-12 15:47:39 -0300

    feat(auth): add bcryptjs for password hashing and update create account logic

--- apps/api/src/http/routes/auth/create-account.ts
+++ apps/api/src/http/routes/auth/create-account.ts
@@ -1,6 +1,9 @@
+import { hash } from 'bcryptjs'
 import { FastifyInstance } from 'fastify'
 import type { ZodTypeProvider } from 'fastify-type-provider-zod'
 import { z } from 'zod'
+
+import { prisma } from '@/lib/prisma'
 
 export async function createAccount(app: FastifyInstance) {
   app.withTypeProvider<ZodTypeProvider>().post(
@@ -14,8 +17,32 @@ export async function createAccount(app: FastifyInstance) {
         }),
       },
     },
-    () => {
-      return 'User created'
+    async (request, reply) => {
+      const { email, name, password } = request.body
+
+      const userWithSameEmail = await prisma.user.findUnique({
+        where: {
+          email,
+        },
+      })
+
+      if (userWithSameEmail) {
+        return reply.status(409).send({
+          message: 'User with this email already exists',
+        })
+      }
+
+      const passwordHash = await hash(password, 8)
+
+      await prisma.user.create({
+        data: {
+          email,
+          name,
+          passwordHash,
+        },
+      })
+
+      return reply.status(201).send()
     },
   )
 }
